30469c606528874b3b6148eec0f2f175cf8d6a84
VitalyMatyushkin <matushkin.vitaliy@gmail.com>
2017-10-20 17:12:36 +0600

    fix

--- sites/all/themes/isa_theme/template.php
+++ sites/all/themes/isa_theme/template.php
@@ -10,3 +10,22 @@
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+function isa_theme_theme() {
+    $items = array();
+
+    $items['user_login'] = array(
+        'render element' => 'form',
+        'path' => drupal_get_path('theme', 'isa_theme') . '/templates',
+        'template' => 'user-login',
+        'preprocess functions' => array(
+            'isa_theme_preprocess_user_login'
+        ),
+    );
+
+    return $items;
+}
+
+function isa_theme_preprocess_user_login(&$vars) {
+    $vars['intro_text'] = t('Login form');
+}
---
+++ sites/all/themes/isa_theme/templates/user-login.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 20.10.17
+ * Time: 16:42
+ */
+?>
+<p><?php print render($intro_text); ?></p>
+<div class="isa_theme-user-login-form-wrapper">
+    <?php print drupal_render_children($form) ?>
+</div>
