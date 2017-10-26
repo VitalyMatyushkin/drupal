1dd4b3977cf0865a531a40162648979e4d01863b
VitalyMatyushkin <matushkin.vitaliy@gmail.com>
2017-10-26 23:45:30 +0600

    new fix

--- sites/all/themes/isa_theme/templates/block--block--2.tpl.php
+++ sites/all/themes/isa_theme/templates/block--block--2.tpl.php
@@ -6,4 +6,7 @@
  * Time: 15:53
  */
 ?>
-<a class="l-region--header--item" href="<?= $GLOBALS['base_url'] ?>/user/logout">Logout</a>
+<span class="l-region--header--title">National Finals Registration Portal</span>
+<span>
+    <a class="l-region--header--item" href="<?= $GLOBALS['base_url'] ?>/user/logout">Logout</a>
+</span>
--- sites/all/themes/isa_theme/templates/views-view-field--participants--page--title-1.tpl.php
+++ sites/all/themes/isa_theme/templates/views-view-field--participants--page--title-1.tpl.php
@@ -6,5 +6,5 @@
  * Time: 13:42
  */
 ?>
-<a href = "participants/edit/<?= $row->nid ?>"><img width="15px" height="15px" src="sites/all/themes/isa_theme/img/edit.svg" /></a>
-<a href = "participants/delete/<?= $row->nid ?>"><img width="11px" height="11px" src="sites/all/themes/isa_theme/img/delete.svg" /></a>
+<a href = "participants/edit/<?= $row->nid ?>"><img title="Edit participant" width="15px" height="15px" src="sites/all/themes/isa_theme/img/edit.svg" /></a>
+<a href = "participants/delete/<?= $row->nid ?>"><img title="Delete participant" width="11px" height="11px" src="sites/all/themes/isa_theme/img/delete.svg" /></a>
