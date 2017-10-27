5ee4596fefcd0572370e7902a02e91eb7f13e99f
VitalyMatyushkin <matushkin.vitaliy@gmail.com>
2017-10-27 10:29:00 +0600

    new fix

--- sites/all/themes/isa_theme/js/isa.js
+++ sites/all/themes/isa_theme/js/isa.js
@@ -1,5 +1,24 @@
-(function($){
-
+(function ($) {
+    $(document).ready(function() {
+        $('#check-participants').live("click",function(){
+            var url = window.location.origin + window.location.pathname + '/check';
+            $.ajax({
+                type: 'POST',
+                url: url,
+                dataType: 'json',
+                success: function (data) {
+                    console.log('ajax success');
+                    $('.error-list').html(data);
+                },
+                complete: function (data) {
+                    console.log('ajax done');
+                },
+                error: function () {
+                    console.log("error");
+                }
+            });
+        });
+    });
 })(jQuery);
 
 function goBack(){
